a033e502f984479a22468798294f963febb83684
kds98 <skyudae@naver.com>
2023-12-12 10:26:00 +0900

    joinForm class

--- src/main/java/com/ll/medium_mission_shimkyudae/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium_mission_shimkyudae/domain/member/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.ll.medium_mission_shimkyudae.domain.member.member.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +13,15 @@ public class MemberController {
     @GetMapping("/join")
     public String showJoin(){
         return "domain/member/member/join";
+    }
+
+    public static class JoinForm {
+        private String username;
+        private String password;
+    }
+
+    @PostMapping("join")
+    public String signup(JoinForm joinForm) {
+        return "redirect:/";
     }
 }
