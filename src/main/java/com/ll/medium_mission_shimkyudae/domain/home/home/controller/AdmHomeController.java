5953d4a57b438a5f135df646e72420837d4e5fcd
kds98 <skyudae@naver.com>
2023-12-13 17:32:49 +0900

    navigation, logout

--- src/main/java/com/ll/medium_mission_shimkyudae/domain/home/home/controller/AdmHomeController.java
+++ src/main/java/com/ll/medium_mission_shimkyudae/domain/home/home/controller/AdmHomeController.java
@@ -2,7 +2,6 @@ package com.ll.medium_mission_shimkyudae.domain.home.home.controller;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class AdmHomeController {
     @GetMapping("")
-    @PreAuthorize("hasRole('ADMIN')")
     public String showMain() {
         return "domain/home/home/adm/main";
+    }
+
+    @GetMapping("/home/about")
+    public String showAbout() {
+        return "domain/home/home/adm/about";
     }
 }
--- src/main/java/com/ll/medium_mission_shimkyudae/global/security/SecurityConfig.java
+++ src/main/java/com/ll/medium_mission_shimkyudae/global/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -18,11 +19,21 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(authorizeRequests ->
-                        authorizeRequests.requestMatchers("/**")
+                        authorizeRequests
+                                .requestMatchers("/gen/**")
+                                .permitAll()
+                                .requestMatchers("/resource/**")
+                                .permitAll()
+                                .requestMatchers("/h2-console/**")
+                                .permitAll()
+                                .requestMatchers("/adm/**")
+                                .hasRole("ADMIN")
+                                .anyRequest()
                                 .permitAll()
                 )
                 .headers(
@@ -44,6 +55,11 @@ public class SecurityConfig {
                                         .loginPage("/member/login")
                                         .defaultSuccessUrl("/?msg=" + URLEncoder.encode("환영합니다.", StandardCharsets.UTF_8))
                                         .failureUrl("/member/login?failMsg=" + URLEncoder.encode("아이디 또는 비밀번호가 틀렸습니다.", StandardCharsets.UTF_8))
+                )
+                .logout(
+                        logout ->
+                                logout
+                                        .logoutRequestMatcher(new AntPathRequestMatcher("/memver/logout"))
                 );
 
         return http.build();
